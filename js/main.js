70e92cb2b3b0754ed5c6ceb710edda78884a925e
Oishi Yuikyo <mahayana3pm@gmail.com>
2020-07-19 18:34:16 +0900

    add second-page

--- js/main.js
+++ js/main.js
@@ -1,19 +1,55 @@
 $(function () {
-	const $moreButton = $("<button>More</button>").attr("id", "more");
-	const $lessButton = $("<button>Less</button>").attr("id", "less");
-
-	$("#description").after($moreButton);
-
-	if ($("#description").height()) {
-		$("#description").hide();
+	const $div1 = $("<div></div>").attr("class", "wrapper");
+	const $div2 = $("<div></div>").attr("class", "wrapper");
+	const $moreButton = $div1.append(
+		$("<button>More</button>").attr("id", "more").attr("class", "more"),
+	);
+	const $lessButton = $div2.append(
+		$("<button>Less</button>").attr("id", "less").attr("class", "less"),
+	);
+	function collapse() {
+		$("#description").css("height", "100px");
+		$("#description").css("white-space", "nowrap");
+		$("#description").css("overflow", "hidden");
+		$("#description").css("text-overflow", "ellipsis");
 	}
 
+	if ($("#description").height() > 300) {
+		collapse();
+		$("#description").after($moreButton);
+	}
 	$(document).on("click", "#more", function () {
-		$("#description").show();
+		$("#description").css("height", "100%");
 		$moreButton.replaceWith($lessButton);
 	});
 	$(document).on("click", "#less", function () {
-		$("#description").hide();
+		collapse();
 		$lessButton.replaceWith($moreButton);
 	});
+
+	// drawer
+	$("#bargerBtn").click(function () {
+		$("#overlay").addClass("active");
+		$("#drawer").addClass("active");
+		$("#overlay").css("height", $("body").outerHeight());
+		$("#drawer").css("height", $("body").outerHeight());
+	});
+	$("#overlay").click(function () {
+		$("#overlay").removeClass("active");
+		$("#drawer").removeClass("active");
+	});
+});
+
+var mySwiper = new Swiper(".swiper-container", {
+	// Optional parameters
+	direction: "horizontal",
+	loop: true,
+	observer: true,
+	observeParents: true,
+
+	// Navigation arrows
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
 });
